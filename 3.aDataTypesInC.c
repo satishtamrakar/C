f49d8ddefe8171a9e0d0878cc29fada9ac0165ce
Satish Tamrakar <45175654+satishtamrakar@users.noreply.github.com>
2018-11-22 08:30:00 +0545

    Add files via upload

---
+++ 3.aDataTypesInC.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+void main ()
+{
+    char a=5,b=3;
+    char c;
+    c=a+b;
+    printf("sum = %d",c);
+    printf("\nThe sum of %d and %d is %d",a,b,c);
+}
---
+++ 3.bDataTypeInC.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    float weight=42.37;
+    printf("%f",weight);
+}
